d80fd096c90d6b597285cda95b5e1cee99a4f47f
Ninh Văn Tuấn Đạt <86716708+Ninh-Dat@users.noreply.github.com>
2022-04-18 21:01:43 +0700

    update login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import {HomeComponent} from './home/home.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './page/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page/login/login.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../service/auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup | undefined;
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(8)]],
+    });
+  }
+  get f() {
+    // @ts-ignore
+    return this.loginForm.controls;
+  }
+  submit() {
+    // @ts-ignore
+    const data = this.loginForm.value;
+    this.authService.login(data).subscribe(res => {
+      const userLogin = res.user;
+      localStorage.setItem('userLogin', JSON.stringify(userLogin));
+      this.router.navigate(['home']);
+    });
+  }
+
+
+}
