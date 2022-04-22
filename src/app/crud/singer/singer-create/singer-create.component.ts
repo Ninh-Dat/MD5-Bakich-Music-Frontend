cff875805e32bfb91f9e65d241354972534eb4b7
Ninh Văn Tuấn Đạt <86716708+Ninh-Dat@users.noreply.github.com>
2022-04-22 09:01:51 +0700

    update validate form singer-create

--- src/app/crud/singer/singer-create/singer-create.component.ts
+++ src/app/crud/singer/singer-create/singer-create.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {SingerService} from '../../../service/singer.service';
 import {Router} from '@angular/router';
 import {CategoryService} from '../../../service/category.service';
@@ -13,24 +13,32 @@ import {ToastrService} from 'ngx-toastr';
 export class SingerCreateComponent implements OnInit {
   categories: any;
   // @ts-ignore
+  singerForm: any
 
-  singerForm: FormGroup = new FormGroup({
-    name: new FormControl(),
-    category_id: new FormControl(),
-    gender: new FormControl(),
-    date_of_birth: new FormControl(),
-    biography: new FormControl(),
-
-
-  });
+  // singerForm: FormGroup = new FormGroup({
+  //   name: new FormControl(),
+  //   category_id: new FormControl(),
+  //   gender: new FormControl(),
+  //   date_of_birth: new FormControl(),
+  //   biography: new FormControl(),
+  //
+  // });
 
   constructor(private singerService: SingerService,
               private router: Router,
               private categoryService: CategoryService,
-              private  toarts: ToastrService) {
+              private  toarts: ToastrService,
+              private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.singerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      category_id: [''],
+      gender: [''],
+      date_of_birth: ['',  [Validators.required]],
+      biography: ['',  [Validators.required]],
+    });
     this.getAllCategory();
   }
 
@@ -50,5 +58,9 @@ export class SingerCreateComponent implements OnInit {
 
   toartrSinger(){
     this.toarts.success('Tọa thành công','Ok')
+  }
+  get f() {
+    // @ts-ignore
+    return this.singerForm.controls;
   }
 }
